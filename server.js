131aa65e2c083218656defb566f3858a8dfba105
JosephWong123 <jjwong3@uci.edu>
2020-08-08 15:25:09 -0700

    updated starter files

--- server.js
+++ server.js
@@ -4,8 +4,16 @@ const express = require('express');
 
 const app = express();
 const server = require('http').createServer(app);
-const socket = require('socket.io')(server);
+const io = require('socket.io')(server);
+
+io.on("connection", (socket) => {
+    console.log("User connected with id " + socket.id);
+});
 
 server.listen(PORT, () => {
     console.log('Server started on ' + PORT);
+});
+
+server.on('error', (err) => {
+    console.error('Server error:', err);
 });
