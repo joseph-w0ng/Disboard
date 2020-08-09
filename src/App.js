09bffcecfb33342805003625cdbc68e8b7707e84
JosephWong123 <jjwong3@uci.edu>
2020-08-08 21:29:47 -0700

    added basic whiteboard

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <Whiteboard className="Board"/>
+        <Whiteboard />
       </div>
     );
   }
--- src/whiteboard.js
+++ src/whiteboard.js
@@ -1,13 +1,86 @@
-import React, {Component} from 'react';
+// Original source:
+
+import React, { useRef, useEffect, useState } from 'react';
 import './whiteboard.css';
 
-class Whiteboard extends Component {
-    render() {
-        return (
-            <canvas className="board">
-            </canvas>
-        )
+function Whiteboard() {
+
+  const canvasRef = useRef(null)
+  const contextRef = useRef(null)
+  const [isDrawing, setIsDrawing] = useState(false)
+  const [isErasing, setIsErasing] = useState(false);
+
+  useEffect(() => {
+    const canvas = canvasRef.current;
+    canvas.width = window.innerWidth * 2;
+    canvas.height = window.innerHeight * 2;
+    canvas.style.width = `${window.innerWidth}px`;
+    canvas.style.height = `${window.innerHeight}px`;
+
+    const context = canvas.getContext("2d")
+    context.scale(2,2)
+    context.lineCap = "round"
+    context.strokeStyle = "black"
+    context.lineWidth = 5
+    contextRef.current = context;
+  }, [])
+
+  const startDrawing = ({nativeEvent}) => {
+    const canvas = canvasRef.current;
+    const context = canvas.getContext("2d")
+    if (isErasing) {
+        context.strokeStyle = "white";
+        context.lineWidth = "20";
     }
+    else {
+        context.strokeStyle = "black";
+        context.lineWidth = 5
+    }
+    const {offsetX, offsetY} = nativeEvent;
+    contextRef.current.beginPath()
+    contextRef.current.moveTo(offsetX, offsetY)
+    setIsDrawing(true)
+  }
+
+  const finishDrawing = () => {
+   contextRef.current.closePath()
+   setIsDrawing(false)
+  }
+
+  const draw = ({nativeEvent}) => {
+    if(!isDrawing){
+      return
+    }
+    const {offsetX, offsetY} = nativeEvent;
+    contextRef.current.lineTo(offsetX, offsetY)
+    contextRef.current.stroke()
+  }
+
+  const setDrawing = () => {
+      setIsErasing(false);
+  }
+
+  const setErasing = () => {
+      setIsErasing(true);
+  }
+
+  return (
+    <div id="whiteboard">
+        <div id="buttons">
+            <button onClick={setDrawing}>Draw</button>
+            <button onClick={setErasing}>Erase</button>
+        </div>
+        <canvas
+        onMouseDown={startDrawing}
+        onTouchStart={startDrawing}
+        onMouseUp={finishDrawing}
+        onTouchEnd={finishDrawing}
+        onTouchMove={draw}
+        onMouseMove={draw}
+        ref={canvasRef}
+        />
+    </div>
+  );
 }
 
 export default Whiteboard;
