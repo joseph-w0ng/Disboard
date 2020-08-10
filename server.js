cc38bd2bfa4460563c13c9c720a76b7ad95ba47c
Brian Huang <bt99huang@gmail.com>
2020-08-09 23:23:02 -0700

    save draw history

--- server.js
+++ server.js
@@ -9,6 +9,7 @@ const socketEvents = require('./socket-events');
 
 // An object to store connected clients and their data
 const connectedClients = {};
+var history = [];
 
 // Serve the static files
 app.use('/css', express.static(`${__dirname}/css`));
@@ -20,9 +21,17 @@ app.get('/', (req, res) => {
 // Socket.io connection handler
 io.on('connection', (socket) => {
   // At this point a client has connected
+  for(data in history) {
+    socket.emit('drawing', history[data]);
+  }
+
+  console.log(history);
   console.log(`A client has connected (id: ${socket.id})`);
 
-  socket.on('drawing', (data) => socket.broadcast.emit('drawing', data));
+  socket.on('drawing', (data) => {
+    history.push(data);
+    socket.broadcast.emit('drawing', data);
+  });
 
   if (!(socket.id in connectedClients)) {
     connectedClients[socket.id] = {};
