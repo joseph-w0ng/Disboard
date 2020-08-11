daab00f4f91985271d3de7f0aef19bbe32a8b68b
JosephWong123 <jjwong3@uci.edu>
2020-08-11 16:01:25 -0700

    added saving canvas as png file

--- js/stuff.js
+++ js/stuff.js
@@ -50,6 +50,11 @@
       lineWidth--;
   });
 
+  $('#save').click(() => {
+    let dataURL = canvasToImage('image/png');
+    $('#save').attr("href", dataURL);
+  });
+
   socket.on('drawing', onDrawingEvent);
 
   socket.on('clear', onClearUpdate);
@@ -158,6 +163,44 @@
         load = false;
     }
   }
+
+function canvasToImage() {
+  //cache height and width        
+  var w = canvas.width;
+  var h = canvas.height;
+
+  var data;
+
+  //get the current ImageData for the canvas.
+  data = context.getImageData(0, 0, w, h);
+
+  //store the current globalCompositeOperation
+  var compositeOperation = context.globalCompositeOperation;
+
+  //set to draw behind current content
+  context.globalCompositeOperation = "destination-over";
+
+  //set background color
+  context.fillStyle = "white";
+
+  //draw background / rect on entire canvas
+  context.fillRect(0,0,w,h);
+
+  //get the image data from the canvas
+  var imageData = canvas.toDataURL("image/png");
+
+  //clear the canvas
+  context.clearRect (0,0,w,h);
+
+  //restore it with original / cached ImageData
+  context.putImageData(data, 0,0);
+
+  //reset the globalCompositeOperation to what it was
+  context.globalCompositeOperation = compositeOperation;
+
+  //return the Base64 encoded data url string
+  return imageData;
+}
 
   $('#createOption').click( () => {
     $('#roomId').attr('disabled', true);
