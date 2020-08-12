64792baf914d471e2476c84b3b12b715d37225c9
Brian Huang <bt99huang@gmail.com>
2020-08-12 06:05:58 -0700

    Rooms update add room create and select functions prevent multiple room of same name saves user room if page refreshed

--- js/stuff.js
+++ js/stuff.js
@@ -1,14 +1,20 @@
 'use strict';
 
-(function() {
+(function () {
 
   var socket = io();
   var canvas = document.getElementsByClassName('whiteboard')[0];
   var colors = document.getElementsByClassName('color');
   var clear = document.getElementById('clear');
+  var room_namefield = document.getElementById('room name');
+  var user_namefield = document.getElementById('name');
+  var createroom = document.getElementById('createOption');
+  var joinroom = document.getElementById('joinOption');
+  var joinbutton = document.getElementById('enterRoom');
+  var errortxt = document.getElementById('errorMsg');
   var context = canvas.getContext('2d');
   var rect = canvas.getBoundingClientRect();
-  var load = true;
+  var firstload = true;
 
   var offx = rect.left;
   var offy = rect.top;
@@ -22,44 +28,65 @@
   canvas.addEventListener('mouseup', onMouseUp, false);
   canvas.addEventListener('mouseout', onMouseUp, false);
   canvas.addEventListener('mousemove', throttle(onMouseMove, 10), false);
-  
-  //Touch support for mobile devices
-  canvas.addEventListener('touchstart', onMouseDown, false);
-  canvas.addEventListener('touchend', onMouseUp, false);
-  canvas.addEventListener('touchcancel', onMouseUp, false);
-  canvas.addEventListener('touchmove', throttle(onMouseMove, 10), false);
 
-  for (var i = 0; i < colors.length; i++){
+  //Touch support for mobile devices
+  // canvas.addEventListener('touchstart', onMouseDown, false);
+  // canvas.addEventListener('touchend', onMouseUp, false);
+  // canvas.addEventListener('touchcancel', onMouseUp, false);
+  // canvas.addEventListener('touchmove', throttle(onMouseMove, 10), false);
+  canvas.addEventListener('touchstart', (e) => {
+    e.preventDefault();
+    onMouseDown(e);
+  }, false);
+  canvas.addEventListener('touchend', (e) => {
+    e.preventDefault();
+    onMouseUp(e);
+  }, false);
+  canvas.addEventListener('touchcancel', (e) => {
+    e.preventDefault();
+    onMouseUp(e);
+  }, false);
+  canvas.addEventListener('touchmove', (e) => {
+    e.preventDefault();
+    throttle(onMouseMove, 10);
+  }, false);
+
+  for (var i = 0; i < colors.length; i++) {
     colors[i].addEventListener('click', onColorUpdate, false);
   }
   clear.addEventListener('click', onClear, false);
 
+  socket.on('join', onJoinEvent);
   socket.on('drawing', onDrawingEvent);
-
   socket.on('clear', onClearUpdate);
+  socket.on('list room options', updateRoomSelect);
 
+  window.addEventListener('load', onLoad, false);
   window.addEventListener('resize', onResize, false);
-  window.addEventListener('load', function(){load = true;console.log('reloaded')}, false);
   onResize();
 
+  function updateOffset() {
+    rect = canvas.getBoundingClientRect();
+    offx = rect.left;
+    offy = rect.top;
+  }
 
-  function drawLine(x0, y0, x1, y1, color, emit){
-    if(color == 'white') {
-        context.clearRect(x0 - 25, y0 - 25, 50, 50);
+  function drawLine(x0, y0, x1, y1, color, emit) {
+    if (color == 'white') {
+      context.clearRect(x0 - 25, y0 - 25, 50, 50);
     } else {
-        context.beginPath();
-        context.moveTo(x0, y0);
-        context.lineTo(x1, y1);
-        context.strokeStyle = color;
-        context.lineWidth = 2;
-        context.stroke();
-        context.closePath();
+      context.beginPath();
+      context.moveTo(x0, y0);
+      context.lineTo(x1, y1);
+      context.strokeStyle = color;
+      context.lineWidth = 2;
+      context.stroke();
+      context.closePath();
     }
 
     if (!emit) { return; }
     var w = canvas.width;
     var h = canvas.height;
-    var rect = canvas.getBoundingClientRect();
 
     socket.emit('drawing', {
       x0: x0 / w,
@@ -70,43 +97,59 @@
     });
   }
 
-  function onMouseDown(e){
+  function onMouseDown(e) {
     drawing = true;
-    current.x = (e.clientX||e.touches[0].clientX) - offx;
-    current.y = (e.clientY||e.touches[0].clientY) - offy;
+    current.x = (e.clientX || e.touches[0].clientX) - offx;
+    current.y = (e.clientY || e.touches[0].clientY) - offy;
   }
 
-  function onMouseUp(e){
+  function onMouseUp(e) {
     if (!drawing) { return; }
     drawing = false;
-    drawLine(current.x, current.y, (e.clientX||e.touches[0].clientX) - offx, (e.clientY||e.touches[0].clientY) - offy, current.color, true);
+    drawLine(current.x, current.y, (e.clientX || e.touches[0].clientX) - offx, (e.clientY || e.touches[0].clientY) - offy, current.color, true);
   }
 
-  function onMouseMove(e){
+  function onMouseMove(e) {
     if (!drawing) { return; }
-    drawLine(current.x, current.y, (e.clientX||e.touches[0].clientX) - offx, (e.clientY||e.touches[0].clientY) - offy, current.color, true);
-    current.x = (e.clientX||e.touches[0].clientX) - offx;
-    current.y = (e.clientY||e.touches[0].clientY) - offy;
+    drawLine(current.x, current.y, (e.clientX || e.touches[0].clientX) - offx, (e.clientY || e.touches[0].clientY) - offy, current.color, true);
+    current.x = (e.clientX || e.touches[0].clientX) - offx;
+    current.y = (e.clientY || e.touches[0].clientY) - offy;
   }
 
-  function onColorUpdate(e){
+  function onColorUpdate(e) {
     current.color = e.target.className.split(' ')[1];
   }
 
   function onClear(e) {
-    onClearUpdate(e, true);
+    onClearUpdate(true);
   }
 
-  function onClearUpdate(e, emit) {
-    context.clearRect(0,0,canvas.width,canvas.height);
-    if(!emit) { return; }
+  function onClearUpdate(emit) {
+    context.clearRect(0, 0, canvas.width, canvas.height);
+    if (!emit) { return; }
     socket.emit('clear', {});
+  }
+
+  function updateRoomSelect(data) {
+    joinroom.innerHTML = '';
+    for (let room in data) {
+      joinroom.innerHTML += '<option value="' + room + '">' + data[room].room_name + '</option>';
+    }
+    if(joinroom.options.length == 0) {
+      joinbutton.hidden = true;
+      joinroom.hidden = true;
+      document.getElementById('roomslabel').innerHTML = 'no active rooms';
+    } else {
+      joinbutton.hidden = false;
+      joinroom.hidden = false;
+      document.getElementById('roomslabel').innerHTML = 'active rooms:';
+    }
   }
 
   // limit the number of events per second
   function throttle(callback, delay) {
     var previousCall = new Date().getTime();
-    return function() {
+    return function () {
       var time = new Date().getTime();
 
       if ((time - previousCall) >= delay) {
@@ -116,24 +159,122 @@
     };
   }
 
-  function onDrawingEvent(data){
+  function onJoinEvent(data) {
+    let details = document.getElementById('room details');
+    let buttons = document.getElementById('buttons');
+    buttons.hidden = true;
+    details.hidden = false;
+    details.innerHTML = 'In room: ' + data.room_name + ', name: ' + data.username;
+    details.innerHTML += '<button id="leave">Leave room</button>';
+    document.getElementById('leave').addEventListener('click',(e) => {
+      socket.emit('leave', {
+        roomID: sessionStorage.roomID
+      });
+      sessionStorage.roomID = null;
+      sessionStorage.username = null;
+      buttons.hidden = false;
+      details.hidden = true;
+      onClearUpdate();
+    }, false);
+    updateOffset();
+    onClearUpdate();
+  }
+
+  function dec2hex(dec) {
+    return dec < 10
+      ? '0' + String(dec)
+      : dec.toString(16);
+  }
+
+  // generateId :: Integer -> String
+  function generateId(len) {
+    var arr = new Uint8Array((len || 40) / 2);
+    window.crypto.getRandomValues(arr);
+    return Array.from(arr, dec2hex).join('');
+  }
+
+  createroom.addEventListener('click', onRoomCreate, false);
+
+  async function onRoomCreate(e) {
+    if (!room_namefield.value || !user_namefield.value) { 
+      errortxt.innerHTML = 'Please fill in both your name and room name.';
+      updateOffset();
+      setTimeout(() => {errortxt.innerHTML = ''; updateOffset();}, 2000);
+      return;
+    }
+    if (await checkname(room_namefield.value)) {
+      errortxt.innerHTML = 'Error: room name taken';
+      updateOffset();
+      setTimeout(() => {errortxt.innerHTML = ''; updateOffset();}, 2000);
+      return;
+    }
+    let id = generateId();
+    socket.emit('create', {
+      room_name: room_namefield.value,
+      roomID: id,
+      username: user_namefield.value
+    });
+    sessionStorage.roomID = id;
+    sessionStorage.username = user_namefield.value;
+  }
+
+  async function checkname(name) {
+    // let url = 'http://localhost:3000/checkname';
+    let url = 'https://hackthis-whiteboard.herokuapp.com/checkname';
+    let res = await fetch(url, {
+      method : 'POST',
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify({name:name})
+    }).then(
+      response => response.json() // .json(), etc.
+      // same as function(response) {return response.text();}
+    )
+    console.log(res.result);
+    return res.result;
+  }
+
+  joinbutton.addEventListener('click', onRoomJoin, false);
+
+  function onRoomJoin(e) {
+    if (!user_namefield.value || joinroom.options.length == 0) {
+      errortxt.innerHTML = 'Please fill in your name.';
+      updateOffset();
+      setTimeout(() => {errortxt.innerHTML = ''; updateOffset();}, 2000);
+      console.log(joinroom);
+      return;
+    }
+    let id = joinroom.options[joinroom.selectedIndex].value;
+    socket.emit('load', {
+      roomID: id,
+      username: user_namefield.value
+    });
+    sessionStorage.roomID = id;
+    sessionStorage.username = user_namefield.value;
+  }
+
+  function onDrawingEvent(data) {
     var w = canvas.width;
     var h = canvas.height;
-    var rect = canvas.getBoundingClientRect();
 
     drawLine(data.x0 * w, data.y0 * h, data.x1 * w, data.y1 * h, data.color);
+  }
+
+  function onLoad() {
+    console.log(sessionStorage);
+    // firstload = true;
+    socket.emit('load', {
+      roomID: sessionStorage.roomID,
+      username: sessionStorage.username
+    });
   }
 
   // make the canvas fill its parent
   function onResize() {
     canvas.width = window.innerWidth;
     canvas.height = window.innerHeight;
-    // console.log('here');
-    if(!load) {
-        socket.emit('resize', {});
-    } else {
-        load = false;
-    }
+    
+    if (firstload) {console.log('here'); firstload = false; return; }
+    socket.emit('resize', {});
   }
 
 })();
--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 
 const express = require('express');
+const bodyParser = require('body-parser');
 
 const app = express();
 const http = require('http').Server(app);
@@ -9,13 +10,14 @@ const socketEvents = require('./socket-events');
 const { count } = require('console');
 
 const setIntervalAsync = (fn, ms) => {
-  fn().then(() => {
-    setTimeout(() => setIntervalAsync(fn, ms), ms);
-  });
+    fn().then(() => {
+        setTimeout(() => setIntervalAsync(fn, ms), ms);
+    });
 };
 
 // An object to store connected clients and their data
 const connectedClients = {};
+const rooms = {};
 
 // Stores previous draw data
 var history = [];
@@ -24,61 +26,161 @@ var counter = 0;
 // Serve the static files
 app.use('/css', express.static(`${__dirname}/css`));
 app.use('/js', express.static(`${__dirname}/js`));
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.json());
+app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', '*');
+    next();
+});
 app.get('/', (req, res) => {
-  res.sendFile(`${__dirname}/index.html`);
+    res.sendFile(`${__dirname}/index.html`);
+});
+
+app.post('/checkname', (req, res) => {
+    let exists = false;
+    for (room in rooms) {
+        if(rooms[room].room_name == req.body.name) {exists = true;}
+    }
+    res.json({result: exists});
 });
 
 // Socket.io connection handler
 io.on('connection', (socket) => {
-  // At this point a client has connected
-  // for(data in history) {
-  //   socket.emit('drawing', history[data]);
-  // }
+    // At this point a client has connected
 
-  setTimeout(function drawload() {
-    if(counter >= history.length) {
-      counter = 0;
-    } else {
-      socket.emit('drawing', history[counter]);
-      // console.log(history[counter]);
-      counter++;
-      setTimeout(drawload, 5);
+    if (!(socket.id in connectedClients)) {
+        socket.join('general');
+        socket.emit('list room options', rooms);
+        connectedClients[socket.id] = {};
     }
-  }, 5);
 
-  console.log(`A client has connected (id: ${socket.id})`);
+    console.log(`A client has connected (id: ${socket.id})`);
 
-  socket.on('drawing', (data) => {
-    if(history.length>6000) {
-      history.shift();
+    socket.on('load', (data) => {
+        if (data.roomID && data.username && rooms[data.roomID]) {
+            socket.join(data.roomID);
+            socket.leave('general');
+            socket.emit('join', {
+                room_name: rooms[data.roomID].room_name,
+                username: data.username
+            });
+            rooms[data.roomID].clients.push(socket.id);
+            connectedClients[socket.id] = data.roomID;
+            let roomhistory = rooms[data.roomID].history;
+
+            animatedDraw(roomhistory);
+            return;
+        }
+        animatedDraw(history);
+        console.log('No previous session / room:'+data.roomID+' does not exist');
+        socket.broadcast.emit('list room options', rooms);
+        socket.emit('list room options', rooms);
+    });
+
+    socket.on('leave', (data) => {
+        rooms[data.roomID].clients.splice(rooms[data.roomID].clients.indexOf(socket.id), 1);
+        if(rooms[data.roomID].clients.length == 0) {
+            delete(rooms[data.roomID]);
+            socket.broadcast.emit('list room options', rooms);
+            socket.emit('list room options', rooms);
+        }
+        connectedClients[socket.id] = {};
+        socket.leave(data.roomID);
+        socket.join('general');
+        animatedDraw(history);
+    });
+
+    function animatedDraw(history) {
+        setTimeout(function drawload() {
+            if (counter >= history.length) {
+                counter = 0;
+            } else {
+                socket.emit('drawing', history[counter]);
+                // console.log(history[counter]);
+                counter++;
+                setTimeout(drawload, 5);
+            }
+        }, 5);
     }
-    history.push(data);
-    socket.broadcast.emit('drawing', data);
-  });
 
-  socket.on('clear', (data) => {
-    history.length = 0;
-    socket.broadcast.emit('clear', {});
-  });
+    socket.on('create', (data) => {
+        let roomID = data.roomID;
+        socket.leave('general');
+        socket.join(data.roomID);
+        socket.emit('join', {
+            room_name: data.room_name,
+            username: data.username
+        });
+        roomInfo = {
+            clients: [],
+            history: [],
+            room_name: '',
+            roomID: roomID
+        };
+        roomInfo.clients.push(socket.id);
+        roomInfo.room_name = data.room_name;
+        rooms[roomID] = roomInfo;
+        connectedClients[socket.id] = roomID;
+        socket.broadcast.emit('list room options', rooms);
+        socket.emit('list room options', rooms);
+    });
 
-  socket.on('resize', (data) => {
-    for(data in history) {
-      socket.emit('drawing', history[data]);
-    }
-  });
+    socket.on('drawing', (data) => {
+        let room = connectedClients[socket.id];
+        if(room && rooms[room]) {
+            if (rooms[room].history.length > 6000) {
+                rooms[room].history.shift();
+            }
+            rooms[room].history.push(data);
+            socket.to(rooms[room].roomID).emit('drawing', data);
+        } else {
+            if (history.length > 6000) {
+                history.shift();
+            }
+            history.push(data);
+            socket.to('general').emit('drawing', data);
+        }
+    });
 
-  if (!(socket.id in connectedClients)) {
-    connectedClients[socket.id] = {};
-  }
+    socket.on('clear', (data) => {
+        let room = connectedClients[socket.id];
+        if(room && rooms[room]) {
+            rooms[room].history.length = 0;
+            socket.to(rooms[room].roomID).emit('clear');
+        } else {
+            history.length = 0;
+            socket.to('general').emit('clear');
+        }
+    });
 
-  socket.on('disconnect', () => {
-    console.log(`Client disconnected (id: ${socket.id})`);
-    delete connectedClients[socket.id];
-  });
+    socket.on('resize', (data) => {
+        let roomhistory = history;
+        let room = connectedClients[socket.id];
+        if(room && rooms[room]) {
+            roomhistory = rooms[room].history;
+        }
+        animatedDraw(roomhistory);
+    });
+
+    socket.on('disconnect', () => {
+        let room = connectedClients[socket.id];
+        if (rooms[room]) {
+            console.log(rooms[room] == false + ' here');
+            rooms[room].clients.splice(rooms[room].clients.indexOf(socket.id), 1);
+            setTimeout(function() {
+                if(rooms[room] && rooms[room].clients.length == 0) {
+                    delete(rooms[room]);
+                    socket.broadcast.emit('list room options', rooms);
+                    socket.emit('list room options', rooms);
+                }}, 3000);
+        }
+        console.log(`Client disconnected (id: ${socket.id})`);
+        delete connectedClients[socket.id];
+    });
 });
 
 // Start the server
 const port = process.env.PORT || 3000;
 http.listen(port, () => {
-  console.log(`Listening on port ${port}`);
+    console.log(`Listening on port ${port}`);
 });
