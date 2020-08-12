46a9b90a80799611f053e9f8f15b46e1be5b7fcc
Brian Huang <bt99huang@gmail.com>
2020-08-12 16:04:42 -0700

    other mobile fix add access control allow headers content type allow mobile endpoint request

--- server.js
+++ server.js
@@ -30,6 +30,9 @@ app.use(bodyParser.urlencoded({extended: true}));
 app.use(bodyParser.json());
 app.use((req, res, next) => {
     res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Headers', 'Content-Type, Access-Control-Allow-Headers, Authorization, X-Requested-With');
+    res.header('Access-Control-Allow-Methods', 'GET,POST,PUT,DELETE,OPTIONS');
+    
     next();
 });
 app.get('/', (req, res) => {
